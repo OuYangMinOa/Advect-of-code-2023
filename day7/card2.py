ded0f1fcc02193833a75ecc307f20cff8b72805c
OuYangMinOa <wesley91345@gmail.com>
2024-06-21 18:07:45 +0800

    day7 complete

---
+++ day7/card2.py
@@ -0,0 +1,104 @@
+from enum import Enum
+from collections import defaultdict
+
+class Strength:
+    FiveKind  = 6
+    FourKind  = 5
+    FullHouse = 4
+    ThreeKind = 3
+    TwoPair   = 2
+    OnePair   = 1
+    HighCard  = 0
+    
+    # count how many same card
+    @staticmethod
+    def count_same(s:str): # return a dictionary
+        output = defaultdict(int)
+        for k in s:
+            output[k] += 1
+        return output
+
+    @classmethod
+    def getEnum(self, s:str):
+        # if s all the same
+        j_num      = s.count('J')
+        s          = s.replace('J','')
+        same_dict  = self.count_same(s) 
+        this_value = sorted(same_dict.values())
+        
+        if (len(this_value) > 0):
+            this_value[-1] += j_num
+        else:
+            this_value.append(j_num)
+
+
+        if ( len(this_value) == 1 and this_value[0] == 5):
+            return Strength.FiveKind 
+        if (len(this_value) == 2):
+            if (this_value[0] == 2 and this_value[1] == 3): # 2 3
+                return Strength.FullHouse 
+            if (this_value[0] == 1 and this_value[1] == 4): # 1 4
+               return Strength.FourKind
+        if (len(this_value) == 3):
+            if (this_value[0] == 1): # 1 1 3 or 1 2 2 
+                if (this_value[2] == 3):
+                    return Strength.ThreeKind
+                if (this_value[2] == 2):
+                    return Strength.TwoPair
+        if (len(this_value) == 4): # 1 1 1 4
+            return Strength.OnePair
+
+        return Strength.HighCard
+
+class Card:
+    CardDict = {
+        'J':0,
+        '2':1,
+        '3':2,
+        '4':3,
+        '5':4,
+        '6':5,
+        '7':6,
+        '8':7,
+        '9':8,
+        'T':9,
+        'Q':11,
+        'K':12,
+        'A':13,
+        }
+                    
+
+    def __init__(self, card:str)->None:
+        self.card = card
+        self.Type = Strength.getEnum(self.card)
+        self.First = self.card_to_num(self.card[0])
+        print(self.card, self.Type, self.First)
+
+    def card_to_num(self,c):
+        return self.CardDict[c]
+
+    def __gt__(self, other):
+        if (self.Type != other.Type):
+            return self.Type > other.Type
+        for k,u in zip(self.card,other.card):
+            if ( self.card_to_num(k) != self.card_to_num(u)):
+                return  self.card_to_num(k) > self.card_to_num(u)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day7/part2.py
@@ -0,0 +1,32 @@
+
+from card2 import Card
+
+def readfile(filename:str):
+    with open(filename, 'r') as f:
+        text = f.read().splitlines()
+    return text
+
+def calculate_answer(arr):
+    output = 0
+    for num, value in enumerate(arr):
+        output += (num+1) * value[1]
+    return output
+
+
+def main():
+    filename = "test2.txt"
+    text_arr = readfile(filename)
+    cards = list( [ each.split(" ")[0] for each in text_arr ])
+    bids  = list( [ int(each.split(" ")[1]) for each in text_arr ])
+    cb_arr = list( zip(cards, bids) )
+    cb_arr.sort(key=lambda x : Card(x[0]))
+    print(f"{cb_arr}")
+    print( calculate_answer(cb_arr) )
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
